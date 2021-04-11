f971f0e37eb01f275994efcbeac15c08d3ab2b5e
Mikhail Bispo <anaomsb@hotmail.com>
2021-04-11 15:10:06 -0300

    Ajuste na conexão com banco e menu

---
+++ Desenvolvimento/VS-2019/Testes_Harmoniza/BD/conexaoBD.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace Testes_Harmoniza.BD
+{
+    public class conexaoBD
+    {
+        private readonly MySqlConnection conexao;
+        public conexaoBD()
+        {
+            conexao = new MySqlConnection("server=localhost; User ID=teste1harmocomque; database=teste1harmocomque; password=TESTE123");
+            conexao.Open();
+        }
+        public void Dispose()
+        {
+            if (conexao.State == System.Data.ConnectionState.Open)
+                conexao.Close();
+        }
+
+        public void executaComando(string strComando)
+        {
+            var vComando = new MySqlCommand
+            {
+                CommandText = strComando,
+                CommandType = System.Data.CommandType.Text,
+                Connection = conexao
+            };
+            vComando.ExecuteNonQuery();
+        }
+
+        public MySqlDataReader retornaComando(string strComando)
+        {
+            var vComando = new MySqlCommand(strComando, conexao);
+
+            return vComando.ExecuteReader();
+        }
+    }
+}
