0e733333af84896e98ae17e43cf9ed7c8da95b47
Mikhail Bispo <anaomsb@hotmail.com>
2021-04-15 23:41:07 -0300

    Criada classes cerveja e cervejaria

---
+++ Desenvolvimento/VS-2019/Testes_Harmoniza/Models/Cerveja.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Testes_Harmoniza.Models
+{
+    public class Cerveja
+    {
+        public int IDCerveja { get; set; }
+        public string Nome { get; set; }
+        public int Estilo { get; set; }
+        public float ABV { get; set; }
+        public int IBU { get; set; }
+        public string Descricao { get; set; }
+        public int Conteudo { get; set; }
+        public string Visual { get; set; }
+        public string Olfativo { get; set; }
+        public string Gustativo { get; set; }
+        public int Temperatura { get; set; }
+    }
+}
