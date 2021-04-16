7a24789dd20971f50e1c026c0c9d75db1d8277cd
Mikhail Bispo <anaomsb@hotmail.com>
2021-04-16 20:36:40 -0300

    Modelos harmonizacao, vinicola e vinho criados

---
+++ Desenvolvimento/VS-2019/Testes_Harmoniza/Models/Harmonizacao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Testes_Harmoniza.Models
+{
+    public class Harmonizacao
+    {
+        public int IDHarmonizacao { get; set; }
+        public string NomeHarmonizacao { get; set; }
+
+    }
+}
